38a73310378da99e5db9e241dbcbce98b4cad024
Javier Rodriguez Posada <jrposada.dev@gmail.com>
2023-04-10 13:39:04 +0200

    Fix arugment types for component reset.

--- lib/utils/component-reset.ts
+++ lib/utils/component-reset.ts
@@ -1,7 +1,10 @@
+import { ComponentType } from 'react'
 import { ComponentMock } from '../component/mock-component'
 
-function componentReset(mock: ComponentMock<any, any>) {
-    mock._fn.mockReset()
+function componentReset<TProps>(component: ComponentType<TProps>) {
+    const componentMock = component as ComponentMock<TProps>
+
+    componentMock._fn.mockReset()
 }
 
 export default componentReset
