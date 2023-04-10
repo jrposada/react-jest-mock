a1d72d3cf4589ec026f35e1d52e776fb38ee4af3
Javier Rodriguez Posada <jrposada.dev@gmail.com>
2023-04-10 13:45:18 +0200

    Export getRef() and getProps() utils.

--- index.ts
+++ index.ts
@@ -7,6 +7,8 @@ import {
 import mockComponent, { ComponentMock } from './lib/component/mock-component'
 import fireCallback from './lib/utils/fire-callback'
 import componentReset from './lib/utils/component-reset'
+import getProps from './lib/utils/get-props'
+import getRef from './lib/utils/get-ref'
 
 declare global {
     namespace jest {
@@ -29,5 +31,5 @@ expect.extend({
     toHaveBeenNthRenderedWith,
 })
 
-export { mockComponent, fireCallback, componentReset }
+export { mockComponent, fireCallback, componentReset, getProps, getRef }
 export type { ComponentMock }
